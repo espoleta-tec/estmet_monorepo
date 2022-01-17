3e186504c26df443b49131ee553d3935a73aa941
lazaroofarrill <lazaroofarrill@gmail.com>
2022-01-17 13:49:16 -0500

    storage need options headers

--- esp32_backend/src/main.cpp
+++ esp32_backend/src/main.cpp
@@ -5,41 +5,41 @@
 #include "pinout.h"
 
 
-//void setup() {
-//    Serial.begin(9600);
-//    if (!Wire.begin()) {
-//        Serial.println("Failed to init i2c interface");
-//    }
-//    pinMode(LED_BUILTIN, OUTPUT);
-//    pinMode(LOGO_LED, OUTPUT);
-//    pinMode(PRO_MINI_RESET, OUTPUT);
-//
-//
-//    Serial.println("update 0");
-//    initFileSystem();
-//    loadConf();
-//    initSDCard();
-//    supplyState = digitalRead(POWER_PIN);
-//    if (supplyState) {
-//        serverSetup();
-//        digitalWrite(LOGO_LED, LOW);
-//    }
-//    resetSetup();
-//
-//    digitalWrite(PRO_MINI_RESET, LOW);
-//    delay(50);
-//    digitalWrite(PRO_MINI_RESET, HIGH);
-//    monitorSetup();
-//}
-//
-//void loop() {
-//}
-
 void setup() {
+    Serial.begin(9600);
+    if (!Wire.begin()) {
+        Serial.println("Failed to init i2c interface");
+    }
     pinMode(LED_BUILTIN, OUTPUT);
+    pinMode(LOGO_LED, OUTPUT);
+    pinMode(PRO_MINI_RESET, OUTPUT);
+
+
+    Serial.println("update 0");
+    initFileSystem();
+    loadConf();
+    initSDCard();
+    supplyState = digitalRead(POWER_PIN);
+    if (supplyState) {
+        serverSetup();
+        digitalWrite(LOGO_LED, LOW);
+    }
+    resetSetup();
+
+    digitalWrite(PRO_MINI_RESET, LOW);
+    delay(50);
+    digitalWrite(PRO_MINI_RESET, HIGH);
+    monitorSetup();
 }
 
 void loop() {
-    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
-    delay(1000);
 }
+
+//void setup() {
+//    pinMode(LED_BUILTIN, OUTPUT);
+//}
+//
+//void loop() {
+//    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+//    delay(1000);
+//}
