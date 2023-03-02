72f5d174967868337d3703e92914809119fb28c6
lazaroofarrill <lazaroofarrill@gmail.com>
2023-03-02 13:35:59 -0500

    changes

--- esp32_backend/src/main.cpp
+++ esp32_backend/src/main.cpp
@@ -1,4 +1,4 @@
-#include <Wire.h>
+#include "Wire.h"
 #include "Arduino.h"
 #include "WebServer/webServerProcess.h"
 #include "Monitor/monitorProcess.h"
@@ -12,9 +12,9 @@ void setup() {
         Serial.println("Failed to init i2c interface");
     }
 
-    while (true) {
-        scanI2C();
-    }
+//    while (true) {
+//        scanI2C();
+//    }
 
     pinMode(LED_BUILTIN, OUTPUT);
     pinMode(LOGO_LED, OUTPUT);
